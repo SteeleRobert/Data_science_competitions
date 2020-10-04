50432302f7b6b2d9bd2679f16f71bc4f70ffe3db
Ted Lewitt <lewitt@usc.edu>
2020-10-03 19:18:26 -0700

    Add files via upload

--- place_users.py
+++ place_users.py
@@ -8,9 +8,9 @@ import pickle
 import numpy as np
 from tqdm import tqdm
 
-with open('user_ratings.pkl', 'rb') as handle:
+with open('muser_ratings.pkl', 'rb') as handle:
     user_dict = pickle.load(handle)
-with open('movie_scores.p', 'rb') as handle1:
+with open('scores.p', 'rb') as handle1:
     movie_dict = pickle.load(handle1)
     #Assumes of the form movieID:subspace score
 
@@ -18,12 +18,12 @@ with open('movie_scores.p', 'rb') as handle1:
 def get_user_score_dict(user_dict,movie_dict):
     user_score_dict={}
     for user in tqdm(user_dict.keys()):
-        users_ratings=user_dict[user][0]
+        users_ratings=user_dict[user]
         
         user_subspace_score = np.array([0.,0.,0.,0.,0.])
-        for movieID,rating in users_ratings.items():
+        for movieID,rating in users_ratings:
             try:
-                movie_subspace_scores = movie_dict[movieID]
+                movie_subspace_scores = movie_dict[int(movieID)]
                 movie_subspace_score=(movie_subspace_scores[0]+movie_subspace_scores[1])/2
                 normalized_rating = (rating-3.53)/1.066
                 user_subspace_score+= normalized_rating * movie_subspace_score
@@ -35,6 +35,5 @@ def get_user_score_dict(user_dict,movie_dict):
     return user_score_dict
 
 user_score_dict= get_user_score_dict( user_dict,movie_dict)     
-import pdb;pdb.set_trace()
-with open('movie_scores.p', 'wb') as handle1:
+with open('user_subspace_scores.pkl', 'wb') as handle1:
     pickle.dump(user_score_dict, handle1, pickle.HIGHEST_PROTOCOL)
